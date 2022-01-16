dde5949a0f946963fb7b930bd253722417e25e10
Kartikey <rawatkari554@gmail.com>
2022-01-16 20:49:44 +0530

    Define a method to find out if a number is prime or not.

---
+++ Day3/Prime.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Prime {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int num = sc.nextInt();
+        System.out.println("The number is " + (isPrime(num) ? "prime" : "not prime"));
+    }
+
+    public static boolean isPrime(int num) {
+        for (int i = 2; i < num; i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+    
+}
