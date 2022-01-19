efb789f1fce4b9ae28b534bd04479ccaa79ac7d2
Kartikey <rawatkari554@gmail.com>
2022-01-19 14:09:20 +0530

    Richest Customer Wealth

---
+++ Day6/RichestCustomerWealth.java
@@ -0,0 +1,29 @@
+// Richest Customer Wealth 
+public class RichestCustomerWealth {
+    public static void main(String[] args) {
+        int[][] accounts = {{1,2,3},{3,2,1}};
+        int ans = maximumWealth(accounts);
+        System.out.println(ans);
+
+
+        
+    }
+
+    public static int maximumWealth(int[][] accounts) {
+        
+        int max=0;
+        for(int i=0;i<accounts.length;i++){
+            int sum=0;
+            for(int j=0;j<accounts[i].length;j++){
+                sum = sum+accounts[i][j];
+                
+            }
+            if(sum>max){
+                max = sum;
+            }
+            
+        }
+        return max;
+    }
+    
+}
